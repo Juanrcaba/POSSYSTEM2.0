76a8abfcc0c685cf6205d56373b54e2c5735a5aa
Juan Santos <juanrscaba@gmail.com>
2021-11-03 21:30:20 -0400

    Add project files.

---
+++ CapaDatos/D_CATEGORIA.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using CapaEntidades;
+using System.Configuration;
+
+namespace CapaDatos
+{
+    public class D_CATEGORIA
+    {
+        SqlConnection conexion = new SqlConnection(ConfigurationManager.ConnectionStrings["conectar"].ConnectionString);
+
+        public DataTable Mostrar(string buscar)
+        {
+            DataTable Dt = new DataTable();
+
+            SqlCommand cmd = new SqlCommand("SP_BUSCARCATEGORIA");
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@BUSCAR",buscar);
+
+            SqlDataAdapter Da = new SqlDataAdapter(cmd);
+            Da.Fill(Dt);
+
+            return Dt;
+        }
+
+        public void InsertarCategoria(E_CATEGORIA categoria)
+        {
+            SqlCommand cmd = new SqlCommand("SP_INSERTARCATEGORIA");
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@NOMBRE",categoria.Nombre);
+            cmd.Parameters.AddWithValue("@DESCRIPCION",categoria.Descripcion);
+
+            conexion.Open();
+            cmd.ExecuteNonQuery();
+            conexion.Close();
+        }
+
+        public void EditartarCategoria(E_CATEGORIA categoria)
+        {
+            SqlCommand cmd = new SqlCommand("SP_EDITARCATEGORIA");
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@IDCATEGORIA",categoria.Idcategoria);
+            cmd.Parameters.AddWithValue("@NOMBRE", categoria.Nombre);
+            cmd.Parameters.AddWithValue("@DESCRIPCION", categoria.Descripcion);
+
+            conexion.Open();
+            cmd.ExecuteNonQuery();
+            conexion.Close();
+        }
+
+        public void EliminarCategoria(E_CATEGORIA categoria)
+        {
+            SqlCommand cmd = new SqlCommand("SP_ELIMINARCATEGORIA");
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@IDCATEGORIA", categoria.Idcategoria);
+         
+            conexion.Open();
+            cmd.ExecuteNonQuery();
+            conexion.Close();
+        }
+    }
+}
---
+++ CapaEntidades/E_CATEGORIA.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class E_CATEGORIA
+    {
+        private int _Idcategoria;
+        private string _Codigo;
+        private string _Nombre;
+        private string _Descripcion;
+
+        public int Idcategoria { get => _Idcategoria; set => _Idcategoria = value; }
+        public string Codigo { get => _Codigo; set => _Codigo = value; }
+        public string Nombre { get => _Nombre; set => _Nombre = value; }
+        public string Descripcion { get => _Descripcion; set => _Descripcion = value; }
+    }
+}
